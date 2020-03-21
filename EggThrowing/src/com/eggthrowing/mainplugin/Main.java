80c7777c3c1b771a6918acb428c29e9bbe87b21d
Nolan Trapp <officialstackemup@gmail.com>
2020-03-21 13:00:50 -0700

    1st update

---
+++ EggThrowing/src/com/eggthrowing/mainplugin/Main.java
@@ -0,0 +1,35 @@
+package com.eggthrowing.mainplugin;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerEggThrowEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import net.md_5.bungee.api.ChatColor;
+
+public class Main extends JavaPlugin implements Listener{
+	
+	@Override
+	public void onEnable() {
+		
+		System.out.println("EggThrowing ENABLED");
+		Bukkit.getPluginManager().registerEvents(this, this);
+	}
+	
+	public void onDisbale() {
+		
+		System.out.println("EggThrowing DISABLED");
+
+	}
+
+	@EventHandler
+	public void onThrow(PlayerEggThrowEvent e) {
+		
+		Player player = e.getPlayer();
+		
+		player.sendMessage(ChatColor.RED + "Egg Thrown!");
+	}
+
+}
